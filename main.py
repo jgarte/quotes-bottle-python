f84f2e0264e2ce4ef0ac8cca029d8923a69eb812
rkshrksh <aarkaygautam@gmail.com>
2019-11-02 10:42:08 +0530

    heroku ready

--- main.py
+++ main.py
@@ -15,4 +15,7 @@ def json_quote():
     }
 
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
